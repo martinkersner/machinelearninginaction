b39bbcf253d47c9ecfbf2b17d1827470f01514de
Martin Kersner <m.kersner@gmail.com>
2017-01-24 19:00:27 +0900

    added script to run svm and made code more readable

---
+++ Ch06/run-svm.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import numpy
+import svmMLiA
+
+dataArr, labelArr = svmMLiA.loadDataSet('testSet.txt')
+b, alphas = svmMLiA.smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+print b
+print alphas[alphas>0]
